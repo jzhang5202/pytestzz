270e8e69a2efa32a744b5f549896fee5ba80d512
jzhang5202 <yujuan99099@163.com>
2022-02-16 11:52:59 +0800

    重新打开后，第六次提交

--- testcases/test_api.py
+++ testcases/test_api.py
@@ -239,7 +239,7 @@ class TestApi:
         #发送请求
         res = requests.request(method=method,url=url,params=paramsInfo['request']['params'])
         print(res.text)
-        print("3333")
+        print("第六次提交")
 
 
 
